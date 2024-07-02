57ee59e202a491cf5abb909d96df8a5734e48bf9
VikkiVignesh <Vikki.Vishnu04@gmail.com>
2024-07-02 16:42:33 +0530

    Vectors

--- Collection_FrameWorks/List/Vectors.java
+++ Collection_FrameWorks/List/Vectors.java
@@ -1,12 +1,27 @@
 package Collection_FrameWorks.List;
 
 import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.Vector;
+
+import java.util.Iterator;
 
 public class Vectors {
     public static void main(String[] args) {
         Vector<String> v=new Vector<>();
         v.addAll(Arrays.asList("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"));
         System.out.println(v);
+
+        System.out.println("Printing Elements Using Enumerations");
+        Enumeration<String> enm=v.elements();
+        while (enm.hasMoreElements()) {
+            System.out.println(enm.nextElement());
+        }
+
+        System.out.println("Printing elements using Iterators ");
+        Iterator<String> it=v.iterator();
+        while (it.hasNext()) {
+          System.out.println(it.next());   
+        }
     }
 }
